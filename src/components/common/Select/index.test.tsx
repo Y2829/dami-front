7d45a006bcd28def647f85e5b57e930e261971cb
kimmeh <mhko0411@gmail.com>
2022-12-05 21:49:16 +0900

    feat: add select with test

---
+++ src/components/common/Select/index.test.tsx
@@ -0,0 +1,47 @@
+import { render, fireEvent } from "@testing-library/react";
+
+import Select from "./index";
+
+type MenuItem = { id: number; value: string; name: string };
+
+describe("Select Component 테스트", () => {
+  const label = "색상";
+  const menuItems: Array<MenuItem> = [
+    { id: 1, value: "Red", name: "빨간색" },
+    { id: 2, value: "Green", name: "초록색" },
+    { id: 3, value: "Blue", name: "파란색" },
+  ];
+  const handleChange = jest.fn();
+
+  it("rendering test", () => {
+    render(
+      <Select label={label} menuItems={menuItems} onChange={handleChange} />,
+    );
+  });
+
+  it("select change test", () => {
+    const { getByDisplayValue } = render(
+      <Select label={label} menuItems={menuItems} onChange={handleChange} />,
+    );
+    const select = getByDisplayValue("");
+    fireEvent.change(select, { target: { value: "Red" } });
+    expect((select as HTMLInputElement).value).toMatch("Red");
+
+    fireEvent.change(select, { target: { value: "Green" } });
+    expect((select as HTMLInputElement).value).toMatch("Green");
+  });
+
+  it("select default value test", () => {
+    const { getByDisplayValue } = render(
+      <Select
+        label={label}
+        menuItems={menuItems}
+        defaultValue={"Blue"}
+        onChange={handleChange}
+      />,
+    );
+
+    const select = getByDisplayValue("Blue");
+    expect((select as HTMLInputElement).value).toMatch("Blue");
+  });
+});
---
+++ src/components/common/Select/index.tsx
@@ -0,0 +1,52 @@
+import type { SelectChangeEvent } from "@mui/material/Select";
+
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import { Select as MuiSelect } from "@mui/material";
+
+type SelectMenuItem<T> = { id: number; value: T; name: string };
+
+interface SelectProps<T> {
+  label: string;
+  menuItems: Array<SelectMenuItem<T>>;
+  defaultValue?: T;
+  onChange: (value: T) => void;
+}
+
+const Select = <T,>({
+  label,
+  menuItems,
+  defaultValue,
+  onChange,
+}: SelectProps<T>) => {
+  const [value, setValue] = useState<T | null>(defaultValue ?? null);
+  const handleChange = (e: SelectChangeEvent) => {
+    const selectedValue = e.target.value as T;
+    setValue(selectedValue);
+    onChange(selectedValue);
+  };
+  return (
+    <Box
+      sx={{
+        width: 150,
+      }}
+    >
+      <FormControl fullWidth>
+        <InputLabel>{label}</InputLabel>
+        <MuiSelect value={value ? `${value}` : ""} onChange={handleChange}>
+          {menuItems.map((item) => (
+            <MenuItem value={`${item.value}`} key={item.id}>
+              {item.name}
+            </MenuItem>
+          ))}
+        </MuiSelect>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default Select;
