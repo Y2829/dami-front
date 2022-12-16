b677c935b5eb238c2a1cf146be56b71e76a37fab
kimmeh <mhko0411@gmail.com>
2022-12-16 15:38:09 +0900

    티어 선택 컴포넌트 추가

---
+++ src/pages/AttiList/components/FilterGroup/FilterItems/TierSelect/index.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+import Select from "src/components/common/Select";
+
+interface TierSelectProps {
+  onSelect: (value: Tier | null) => void;
+}
+
+type Tier =
+  | "IRON"
+  | "BRONZE"
+  | "SILVER"
+  | "GOLD"
+  | "PLATINUM"
+  | "DIAMOND"
+  | "MASTER"
+  | "GRAND-MASTER"
+  | "CHALLENGER";
+
+const menuItems: Array<{ id: number; value: Tier; name: string }> = [
+  { id: 1, value: "IRON", name: "아이언" },
+  { id: 2, value: "BRONZE", name: "브론즈" },
+  { id: 3, value: "SILVER", name: "실버" },
+  { id: 4, value: "GOLD", name: "골드" },
+  { id: 5, value: "PLATINUM", name: "플래티넘" },
+  { id: 6, value: "DIAMOND", name: "다이아몬드" },
+  { id: 7, value: "MASTER", name: "마스터" },
+  { id: 8, value: "GRAND-MASTER", name: "그랜드마스터" },
+  { id: 9, value: "CHALLENGER", name: "챌린저" },
+];
+
+export default function TierSelect({ onSelect }: TierSelectProps) {
+  const [value, setValue] = useState<Tier | null>(null);
+
+  useEffect(() => {
+    onSelect(value);
+  }, [value]);
+
+  return (
+    <Select
+      label="티어"
+      value={value}
+      onChange={(newValue) => setValue(newValue)}
+      menuItems={menuItems}
+    />
+  );
+}
